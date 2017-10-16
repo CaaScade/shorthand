14142651f9f8134c2a3aa0bc1efa8a67a27c6ece
Kynan Rilee <kynan.rilee@gmail.com>
2017-10-15 17:46:12 -0700

    yamling

--- main.go
+++ main.go
@@ -1,48 +1,46 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
+	//"bytes"
+	//"encoding/json"
 	"fmt"
 	//"github.com/davecgh/go-spew/spew"
+	"github.com/ghodss/yaml"
+	"github.com/kr/pretty"
+	"io/ioutil"
 	"log"
+	"os"
 )
 
 func main() {
+	inputFile := os.Args[1]
+	content, err := ioutil.ReadFile(inputFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	v := map[string]interface{}{}
-	blob := `{"doot":{},"boop":{"wat":"yes"}}`
-	if err := json.Unmarshal([]byte(blob), &v); err != nil {
+	err = yaml.Unmarshal(content, &v)
+	if err != nil {
 		log.Fatal(err)
 	}
 
-	var out bytes.Buffer
-	if err := json.Indent(&out, []byte(blob), "", "  "); err != nil {
+	_, err = pretty.Println(v)
+	if err != nil {
 		log.Fatal(err)
 	}
 
-	blob1 := out.String()
-	fmt.Println(blob1)
-	//p := MkPattern(MkField("boop", MkField("wat")))
-	p := MkP(P{"boop": P{"wat": nil}})
+	p := MkP(P{"spec": P{"selector": P{"app": nil}}})
 	p.Match(v)
-	//spew.Dump(p)
-	fmt.Println(p.Extract())
+	p.Erase(v)
 
-	v1 := p.Erase(v)
-	blob2, err2 := json.MarshalIndent(v1, "", "  ")
-	if err2 != nil {
-		log.Fatal(err2)
-	}
-	fmt.Println(string(blob2))
-	//spew.Dump(v)
+	q := MkP(P{"spec": P{"app": "doot"}})
+	q.Write(v)
 
-	//q := MkPattern(MkField("b", ValField("p", 1000)))
-	q := MkP(P{"b": P{"p": 1000}})
-	v2 := q.Write(v1)
-	blob3, err3 := json.MarshalIndent(v2, "", "  ")
-	if err3 != nil {
-		log.Fatal(err3)
+	o, err1 := yaml.Marshal(v)
+	if err1 != nil {
+		log.Fatal(err1)
 	}
-	fmt.Println(string(blob3))
-	//spew.Dump(v2)
+
+	fmt.Println(string(o))
 }
